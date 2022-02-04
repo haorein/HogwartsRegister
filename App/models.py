3ff84924be5cc092f615ee190097d1870c2ef755
haorein <donghao0212@gmail.com>
2022-02-04 23:04:09 +0100

    model's fields allow blank

--- App/models.py
+++ App/models.py
@@ -12,11 +12,11 @@ class House(models.Model):
 
 
 class Student(models.Model):
-    name = models.CharField(db_column="Name", max_length=32)
-    surname = models.CharField(db_column="Surname", max_length=32)
-    gender = models.BooleanField(db_column="Gender", default=True)  # True male, False female
-    age = models.IntegerField(db_column="Age", default=1)
-    photo = models.ImageField(db_column="Photo", upload_to="student_photos")
+    name = models.CharField(db_column="Name", max_length=32, blank=True)
+    surname = models.CharField(db_column="Surname", max_length=32, blank=True)
+    gender = models.BooleanField(db_column="Gender", default=True, blank=True)  # True male, False female
+    age = models.IntegerField(db_column="Age", default=1, blank=True)
+    photo = models.ImageField(db_column="Photo", upload_to="student_photos", blank=True)
     s_house = models.ForeignKey(House, on_delete=models.PROTECT)
 
 
@@ -26,7 +26,7 @@ class MistakeRecord(models.Model):
 
 
 class Wand(models.Model):
-    name = models.CharField(db_column="Name", max_length=32)
+    name = models.CharField(db_column="Name", max_length=32, blank=True)
     w_student = models.ForeignKey(Student, on_delete=models.PROTECT)
 
 
@@ -35,6 +35,6 @@ class Magic(models.Model):
         ('N', 'Normal'),
         ('F', 'Forbidden'),
     )
-    type = models.CharField(db_column="Type", max_length=12, choices=TYPES)
-    name = models.CharField(db_column="Name", max_length=32)
+    type = models.CharField(db_column="Type", max_length=12, choices=TYPES, blank=True)
+    name = models.CharField(db_column="Name", max_length=32, blank=True)
     m_wand = models.ForeignKey(Wand, on_delete=models.PROTECT)
