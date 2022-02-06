6f1c50ad5efb07b97d1bcfe5ecc8fcc91b4e20f9
haorein <donghao0212@gmail.com>
2022-02-06 14:43:51 +0100

    set delete option to cascade

--- App/models.py
+++ App/models.py
@@ -17,17 +17,17 @@ class Student(models.Model):
     gender = models.BooleanField(db_column="Gender", default=True, blank=True)  # True male, False female
     age = models.IntegerField(db_column="Age", default=1, blank=True)
     photo = models.ImageField(db_column="Photo", upload_to="student_photos", blank=True)
-    s_house = models.ForeignKey(House, on_delete=models.PROTECT)
+    s_house = models.ForeignKey(House, on_delete=models.CASCADE)
 
 
 class MistakeRecord(models.Model):
     record = models.TextField(db_column="Record", max_length=2000, blank=True)
-    m_student = models.ForeignKey(Student, on_delete=models.PROTECT)
+    m_student = models.ForeignKey(Student, on_delete=models.CASCADE)
 
 
 class Wand(models.Model):
     name = models.CharField(db_column="Name", max_length=32, blank=True)
-    w_student = models.ForeignKey(Student, on_delete=models.PROTECT)
+    w_student = models.ForeignKey(Student, on_delete=models.CASCADE)
 
 
 class Magic(models.Model):
@@ -37,4 +37,4 @@ class Magic(models.Model):
     )
     type = models.CharField(db_column="Type", max_length=12, choices=TYPES, blank=True)
     name = models.CharField(db_column="Name", max_length=32, blank=True)
-    m_wand = models.ForeignKey(Wand, on_delete=models.PROTECT)
+    m_wand = models.ForeignKey(Wand, on_delete=models.CASCADE)
